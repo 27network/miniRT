ab0e4956073f624be8aa4b5b1da197ff08a0cd96
xtrm <oss@xtrm.me>
2024-04-19 01:29:06 +0200

    ✨ feature(flake.nix): added nix flake + .envrc

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "dev-env for 42 projects";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils
+  }: flake-utils.lib.eachDefaultSystem
+    (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.clang17Stdenv;
+      in
+      {
+        devShell = (pkgs.mkShell.override { inherit stdenv; }) {
+          buildInputs = with pkgs; [
+            SDL2
+            readline
+          ];
+          nativeBuildInputs = with pkgs; [
+            norminette
+            valgrind
+            gdb
+          ];
+        };
+      }
+    );
+}
+# vim: ts=2 sw=2 et
+
